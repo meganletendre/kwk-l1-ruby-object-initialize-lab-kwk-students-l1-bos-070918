8996d28a2232ff53b3b06339c6e6e8b65dd6f729
meganletendre <megan.letendre@comcast.net>
2018-07-13 15:09:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,9 @@
 # dog.rb
 class Dog 
-  def initialize(name,breed)
+  def initialize(name,breed = "Mutt")
     @name = name 
-    @breed = "Mutt"
+    @breed = breed
   end 
 end 
+
+fido = Dog.new("Fido","Pug")
